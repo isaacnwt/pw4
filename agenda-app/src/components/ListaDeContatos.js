7f42491a8b2e4a332a7bf1d2e4f9cdcc179f614d
isaacnwt <isaacnewton02@hotmail.com>
2024-09-17 20:05:33 -0300

    feat: salvando numero de telefone

--- agenda-app/src/components/ListaDeContatos.js
+++ agenda-app/src/components/ListaDeContatos.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import "./ListaDeContatos.css";
 
 function ListaDeContatos() {
-  const [listaContatos, setListaContatos] = useState(["Irineu"]);
-  const [contato, setContato] = useState("");
+  const [listaContatos, setListaContatos] = useState([{ nome: "Irineu", telefone: "9999-9999" }]);
+  const [contato, setContato] = useState({ nome: "", telefone: "" });
 
   const handleChange = (event) => {
-    setContato(event.target.value);
+    const { name, value } = event.target;
+    setContato((prevContato) => ({
+      ...prevContato,
+      [name]: value,
+    }));
   };
 
   const handleKeyPress = (event) => {
@@ -16,26 +20,38 @@ function ListaDeContatos() {
   };
 
   const addContato = () => {
-    if (contato.trim() !== '') {
+    if (contato.nome.trim() !== "" && contato.telefone.trim() !== "") {
       setListaContatos([...listaContatos, contato]);
-      setContato('');
+      setContato({ nome: "", telefone: "" });
     }
   };
+
 
   return (
     <div className="card">
       <h1>Lista de Contatos</h1>
       <input
         type="text"
-        value={contato}
+        name="nome"
+        value={contato.nome}
         onChange={handleChange}
         onKeyPress={handleKeyPress}
-        placeholder="Novo contato"
+        placeholder="Nome do contato"
+      />
+      <input
+        type="text"
+        name="telefone"
+        value={contato.telefone}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        placeholder="Telefone"
       />
       <button onClick={addContato}>Adicionar</button>
       <ul>
-        {listaContatos.map((tarefa, index) => (
-          <li key={index}>{tarefa}</li>
+        {listaContatos.map((contato, index) => (
+          <li key={index}>
+            {contato.nome} - {contato.telefone}
+          </li>
         ))}
       </ul>
     </div>
